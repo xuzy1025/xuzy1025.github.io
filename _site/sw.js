975c077f89f0b3ced7c98bcb80eac5d99643c6d0
XUZY <740948432@qq.com>
2024-01-17 16:23:53 +0800

    个人博客搭建

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705475136';
+const cacheName = 'chirpy-1705479796';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
